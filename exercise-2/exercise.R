3784f4a5783a511d9ce5750dc44537edf8004150
Mario Paolini <trepaolini@gmail.com>
2017-01-12 14:52:40 -0800

    More in class work

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,10 +3,16 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
+CompareLength <- function(one,two){
+  length <- (nchar(one) - nchar(two))
+  if(length < 0){
+    length <- -length;
+  }
+  return(paste("The difference in length is ", length))
+}
 
 # Pass two strings of different lengths to your `CompareLength` function
-
+welp <- CompareLength("Wow","OHMAN")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
